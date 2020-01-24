b0f812d0d3b75e17c24fa57ab7b29b271196d2d7
sahithyagadde <39362921+sahithyagadde@users.noreply.github.com>
2020-01-24 13:02:17 -0600

    Create operations.py

---
+++ icp1/operations.py
@@ -0,0 +1,8 @@
+num1= int(raw_input('Enter a number'))
+num2= int(raw_input('Enter another number'))
+
+print('Sum of two numbers is ', num1+num2)
+print('Product of two numbers is ', num1*num2)
+print('Difference of two numbers is ', num1-num2)
+print('Square of first num is ', num1*num1)
+print('Divide number 1 with number 2 ', num1/num2)
